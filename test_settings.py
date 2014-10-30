5368e0ad7be4cdf7df2da392fdaabb89c3a4ad55
Kristian Glass <git@doismellburning.co.uk>
2014-10-30 16:34:52 +0000

    Add missing test settings (in-memory sqlite3 db + SITE_ID)

--- test_settings.py
+++ test_settings.py
@@ -3,3 +3,12 @@ SECRET_KEY = "lorem ipsum"
 INSTALLED_APPS = (
     'tango_shared',
 )
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+SITE_ID = 1
