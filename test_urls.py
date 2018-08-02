dce47f14ac57d4be9889223062f304cd54c6ad93
TBaxter <mail.baxter@gmail.com>
2018-08-02 16:15:48 -0500

    added basic template view for home

--- test_urls.py
+++ test_urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
+from .models import Gallery
 
 urlpatterns = [
-    
+    path('', TemplateView.as_view(template_name="index.html"), name="home"),
 ]
