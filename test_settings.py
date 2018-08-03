2e7cdd53ea7652b45bf100c22ce3fe4408b27f24
TBaxter <mail.baxter@gmail.com>
2018-08-03 08:02:18 -0500

    why request wonky?

--- test_settings.py
+++ test_settings.py
@@ -18,14 +18,14 @@ DATABASES = {
 ROOT_URLCONF = 'test_urls'
 SITE_ID = 1
 
-#stripped down middleware
 MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    #'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'tango_shared.context_processors.site_processor'
-
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 TEMPLATES = [
