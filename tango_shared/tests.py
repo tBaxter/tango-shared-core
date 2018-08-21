e8521457c59e4b98aa90af607872727d6df0d3e1
TBaxter <mail.baxter@gmail.com>
2018-08-21 09:44:21 -0500

    attempting to fix tests for python3

--- tango_shared/tests.py
+++ tango_shared/tests.py
@@ -34,8 +34,8 @@ class TestSharedContent(TestCase):
         favicon_url = '<link rel="shortcut icon" href="{}img/favicon.png">'.format(static_url)
         touch_icon = '<link rel="apple-touch-icon" href="{}img/touch-icon.png">'.format(static_url)
         self.assertEqual(response.status_code, 200)
-        self.assertTrue(favicon_url in response.content)
-        self.assertTrue(touch_icon in response.content)
+        self.assertTrue(favicon_url in response.data)
+        self.assertTrue(touch_icon in response.data)
 
     def test_page_resolution(self):
         response = self.client.get(reverse('home'))
